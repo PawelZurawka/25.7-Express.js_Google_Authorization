0aa75b054953b5d6dafccfe5b3d59b54347483a0
Paweł Żurawka <pzurawka@gmail.com>
2019-08-13 13:35:09 +0200

    Init project

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID':
+    '127348008293-s0usmqqpjn9ccsvm0pcfs5kodckfhsd5.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'hA2nyPbCHYUqTUM4Ep8JrP5R',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
